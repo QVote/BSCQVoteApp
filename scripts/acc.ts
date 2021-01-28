96afbec1d0faf775921cdc6f86d7191e4f92ad46
MszBednarski <msz.bednarski@gmail.com>
2021-01-28 16:18:58 +0000

    this works.

---
+++ scripts/acc.ts
@@ -0,0 +1,22 @@
+import { ethers } from 'ethers';
+import * as fs from 'fs';
+
+const accPath = `${__dirname}/.privKey`;
+
+try {
+    fs.readFileSync(accPath, 'utf8');
+    console.log("An account exists");
+} catch (e) {
+    console.log("The account does not exist, creating new one...");
+    const wallet = ethers.Wallet.createRandom();
+    fs.writeFileSync(accPath, wallet.privateKey);
+}
+
+const privKey = fs.readFileSync(accPath, 'utf8').toString().trim();;
+
+export const bscProvider = new ethers.providers.JsonRpcProvider(`https://data-seed-prebsc-1-s1.binance.org:8545`);
+const signer = new ethers.Wallet(privKey, bscProvider);
+
+console.log(signer.address)
+
+export { signer };
