5c9f7b9a330556a985932a955848d4fed0589e23
MszBednarski <msz.bednarski@gmail.com>
2021-01-30 16:43:53 +0000

    Add voting type.

--- frontend/types/index.d.ts
+++ frontend/types/index.d.ts
@@ -4,6 +4,20 @@ export module QVBSC {
         uid: string,
     }
 
+    export type VotingOption = {
+        optName: string,
+        uid: string,
+        credits: number
+    }
+
+    export type VotingDecision = {
+        name: string,
+        description: string,
+        options: VotingOption[],
+        endTime: number,
+        credits: number
+    }
+
     export type Decision = {
         name: string,
         description: string,
