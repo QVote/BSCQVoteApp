d7a14584a1b753fc5ec3810518640511b13cf4dd
MszBednarski <msz.bednarski@gmail.com>
2021-01-31 10:09:28 +0000

    export abi bytecode.

--- frontend/config/index.ts
+++ frontend/config/index.ts
@@ -1,13 +1,13 @@
-//import abi here
+import { abi, bytecode } from './QVoting.json'
 
 const _config = {
-  NETWORK_NAME: "Binance Smart Chain Testnet",
-  RPC_URL: "https://data-seed-prebsc-1-s1.binance.org:8545/",
-  CHAIN_ID: "97",
-  ERRORS: {
-    WRONG_CHAIN: "WRONG_CHAIN",
-  }
+    NETWORK_NAME: "Binance Smart Chain Testnet",
+    RPC_URL: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+    CHAIN_ID: "97",
+    ERRORS: {
+        WRONG_CHAIN: "WRONG_CHAIN",
+    }
 }
 
-// export { abi };
+export { abi, bytecode };
 export default _config;
